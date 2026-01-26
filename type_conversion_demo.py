3a0a65603475f1debabbc520fb98e015915e1992
h2956 <2956144285@qq.com>
2026-01-26 10:02:54 +0800

    新增数据类型转换代码，包含str转int、int转float等常见类型转换示例

---
+++ type_conversion_demo.py
@@ -0,0 +1,20 @@
+#将数据类型转换成字符串
+num = str(11)
+print(type(num),num)
+#将字符串转换成数字
+str_num = int("123")
+print(type(str_num),str_num)
+#想要将字符串转换成数字，字符串内容必须是数字
+#str_num = int("字符串")
+#print(type(str_num),str_num)
+#整数转浮点数
+num1 = float(11)
+print(type(num1),num1)
+#浮点数转整数
+num2 = int(12.34)
+print(type(num2),num2)
+#整数字符串连着打印
+name1 = "小克的塔罗会编号是"
+#print(type(name1),name1)
+name2 = str(8)
+print(name1,name2)
