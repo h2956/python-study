500132414c23fb92e6451fe19996bafe205f28bd
h2956 <2956144285@qq.com>
2026-01-30 13:04:46 +0800

    add:运算符练习代码

---
+++ operator_practice.py
@@ -0,0 +1,21 @@
+# 加，减，乘，除
+num = 1
+num += 1
+print("num += 1: ", num)
+num -= 1
+print("num -= 1: ", num)
+num *= 2
+print("num *= 2: ", num)
+num /= 2
+print("num /= 2: ", num)
+
+# 取整数，取模，幂运算
+num = 4
+num //= 2
+print("num //= 2: ", num)
+
+num = 3
+num %= 2
+print("num %= 2: ", num)
+num **= 2
+print("num **= 2: ", num)
